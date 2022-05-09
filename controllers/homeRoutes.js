e35a63968b1f7cf8222f1eea553a6d3e93505652
M A Razvi <abuzarflw@gmail.com>
2022-05-08 23:22:13 -0700

    Make main handlebars page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const { User, BlogPost, Comment } = require("../models");
 const router = require("express").Router();
 
 router.get("/", (req,res) => {
-    res.send("Michael Jackson");
+    res.render("mainmain");
 });
 
 module.exports = router;

8254073e871d37ac340d8c87451d7213f091d5be
M A Razvi <abuzarflw@gmail.com>
2022-05-09 23:54:27 -0700

    Create route that shows a post when you click on it

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,17 +3,59 @@ const { User, BlogPost, Comment } = require("../models");
 
 const router = require("express").Router();
 
+//Get login page
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+    res.render('login');
+  });
+  
+
+
+//GET homepage
 router.get("/", async (req,res) => {
-    const allPosts= await BlogPost.findAll({
-        include: [User]
-    })
-
-    const posts = allPosts.map((post) => {
-        return post.get({plain: true});
-    })
-
-    console.log(`-----------------------------!!!!!!!!!!!!! `+ allPosts);
-    res.render("homepage", {posts});
+    try {
+        const allPosts= await BlogPost.findAll({
+            include: [User]
+        })
+        const posts = allPosts.map((post) => post.get({ plain: true }));
+        
+        res.render("homepage", {posts, loggedIn: req.session.loggedIn});
+    }
+     catch (error) {
+        res.status(500).json(error);
+    }
 });
+
+//GET post by id
+router.get('/post/:id', async (req, res) => {
+    try {
+        const specificPost = await BlogPost.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ["id", "title", "description", "user_id"],
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"]
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "content", "user_id", "blogPost_id"]
+                }
+            ]
+        })
+
+        post = specificPost.get({plain: true});
+        res.render("Comment", {post, loggedIn: req.session.loggedIn})  
+
+    } catch (error) {
+        
+    }
+
+})
 
 module.exports = router;

0fb79430cc1829c0a454bdb163c4b92eb7efeefb
M A Razvi <abuzarflw@gmail.com>
2022-05-09 19:32:29 -0700

    Add functionality to add posts to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,8 +3,17 @@ const { User, BlogPost, Comment } = require("../models");
 
 const router = require("express").Router();
 
-router.get("/", (req,res) => {
-    res.render("homepage");
+router.get("/", async (req,res) => {
+    const allPosts= await BlogPost.findAll({
+        include: [User]
+    })
+
+    const posts = allPosts.map((post) => {
+        return post.get({plain: true});
+    })
+
+    console.log(`-----------------------------!!!!!!!!!!!!! `+ allPosts);
+    res.render("homepage", {posts});
 });
 
 module.exports = router;
